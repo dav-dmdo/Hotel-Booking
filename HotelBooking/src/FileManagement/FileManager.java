b2948ce5a0a002dafff42219b94bf6e5a1365de3
dav-dmdo <david.davila@correo.unimet.edu.ve>
2023-06-11 19:08:04 -0400

    Estructura inicial del proyecto

---
+++ HotelBooking/src/FileManagement/FileManager.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package FileManagement;
+
+/**
+ *
+ * @author david
+ */
+public class FileManager {
+    
+    public void readXLSX(){
+        
+    }
+    
+    public void writeXLSX(){
+        
+    }
+    
+    
+}
