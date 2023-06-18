0a96cc0bb684e79a3c04f5b43f1676916cff2f2a
dav-dmdo <david.davila@correo.unimet.edu.ve>
2023-06-18 17:37:19 -0400

    Modifing RegisterClientGUI file on purpose

--- HotelBooking/src/GUIS/RegisterClientGUI.java
+++ HotelBooking/src/GUIS/RegisterClientGUI.java
@@ -21,6 +21,8 @@ public class RegisterClientGUI extends javax.swing.JFrame {
         this.setResizable(false);
     }
 
+    
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
