3c58b40abcc96d43d61e612f18c76e4fe63c9797
AndresEspositoDe <98119676+AndresEspositoDe@users.noreply.github.com>
2023-06-19 13:31:25 -0400

    ✨ Actualizacion parte 1

--- HotelBooking/src/DS/HashTable.java
+++ HotelBooking/src/DS/HashTable.java
@@ -17,39 +17,47 @@ import javax.swing.JTextField;
  */
 public class HashTable {
 
-    private int capacity;
-    private Object[] keys;
-    private Object[] values;
+    private int capacity;        // Capacidad máxima de la tabla
+    private Object[] keys;       // Arreglo para almacenar las claves
+    private Object[] values;     // Arreglo para almacenar los valores correspondientes a cada clave
 
+    // Constructor que recibe la capacidad de la tabla
     public HashTable(int capacity) {
         this.capacity = capacity;
-        this.keys = new Object[capacity];
-        this.values = new Object[capacity];
+        this.keys = new Object[capacity];    // Se crea un arreglo de claves vacío con la capacidad dada
+        this.values = new Object[capacity];  // Se crea un arreglo de valores vacío con la capacidad dada
     }
 
+    // Método para insertar un par clave-valor en la tabla
     public void insert(Object key, Object value) {
-        int hash1 = Math.abs(key.hashCode() % capacity);
-        int hash2 = Math.abs(key.hashCode() % (capacity - 2) + 1);
+        int hash1 = Math.abs(key.hashCode() % capacity);    // Se calcula la posición en la tabla hash usando el hashcode de la clave
+        int hash2 = Math.abs(key.hashCode() % (capacity - 2) + 1);  // Se calcula un segundo hash para manejar colisiones
 
+        // Se busca una posición libre en la tabla, usando el segundo hash para saltar a posiciones diferentes
         while (keys[hash1] != null && !keys[hash1].equals(key)) {
             hash1 = (hash1 + hash2) % capacity;
         }
 
+        // Se inserta la clave y el valor en la posición encontrada
         keys[hash1] = key;
         values[hash1] = value;
     }
 
+    // Método para obtener el valor correspondiente a una clave en la tabla
     public Object get(Object key) {
-        int hash1 = Math.abs(key.hashCode() % capacity);
-        int hash2 = Math.abs(key.hashCode() % (capacity - 2) + 1);
+        int hash1 = Math.abs(key.hashCode() % capacity);    // Se calcula la posición en la tabla hash usando el hashcode de la clave
+        int hash2 = Math.abs(key.hashCode() % (capacity - 2) + 1);  // Se calcula un segundo hash para manejar colisiones
 
+        // Se busca la posición correspondiente a la clave, usando el segundo hash para saltar a posiciones diferentes
         while (keys[hash1] != null && !keys[hash1].equals(key)) {
             hash1 = (hash1 + hash2) % capacity;
         }
 
+        // Se devuelve el valor correspondiente a la clave
         return values[hash1];
     }
 
+    // Método para recorrer la tabla y mostrar las claves y valores almacenados
     public void traverse() {
         for (int i = 0; i < capacity; i++) {
             if (keys[i] != null) {
@@ -58,14 +66,17 @@ public class HashTable {
         }
     }
 
+    // Método para buscar una clave en la tabla y devolver un mensaje que indique si está o no
     public String findKey(Object key) {
-        int hash1 = Math.abs(key.hashCode() % capacity);
-        int hash2 = Math.abs(key.hashCode() % (capacity - 2) + 1);
+        int hash1 = Math.abs(key.hashCode() % capacity);    // Se calcula la posición en la tabla hash usando el hashcode de la clave
+        int hash2 = Math.abs(key.hashCode() % (capacity - 2) + 1);  // Se calcula un segundo hash para manejar colisiones
 
+        // Se busca la posición correspondiente a la clave, usando el segundo hash para saltar a posiciones diferentes
         while (keys[hash1] != null && !keys[hash1].equals(key)) {
             hash1 = (hash1 + hash2) % capacity;
         }
 
+        // Si se encontró la clave, se devuelve un mensaje con su valor asociado; de lo contrario, se devuelve un mensaje de que no se encontró
         if (keys[hash1] != null && keys[hash1].equals(key)) {
             return "El usuario: " + key + " esta hospedado en la habitacion: " + values[hash1];
         } else {
@@ -73,37 +84,43 @@ public class HashTable {
         }
     }
 
+    // Método para leer datos desde un archivo y almacenarlos en la tabla
     public void read(String filename, String a, JTextArea b, JTextField c) {
-        HashTable hashtable = new HashTable(1000);
+        HashTable hashtable = new HashTable(1000);   // Se crea una tabla hash vacía
 
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            BufferedReader reader = new BufferedReader(new FileReader(filename));   // Se crea un lector para el archivo
             String line;
             int cont = 0;
-            while ((line = reader.readLine()) != null) {
-                String[] data = line.split(",");
-                String key = data[1].trim() + " " + data[2].trim();
-                String value = data[0].trim();
-                hashtable.insert(key, value);
+            while ((line = reader.readLine()) != null) {   // Se lee el archivo línea por línea
+                String[] data = line.split(",");   // Se separan los datos por comas
+                String key = data[1].trim() + " " + data[2].trim();   // Se crea una clave a partir de los datos
+                String value = data[0].trim();   //Se obtiene el valor correspondiente a la clave en los datos
+
+                hashtable.insert(key, value);   // Se inserta la clave y el valor en la tabla hash
+
+                // Si la clave coincide con una cadena de texto dada, se muestra el valor correspondiente en un JTextArea
                 if (key.equals(c.getText())) {
                     b.setText("El usuario: " + key + " esta hospedado en la habitacion: " + value);
+                    
                 } else {
-                    cont++;
+                    cont++;   // Si no coincide, se aumenta un contador
                 }
-
             }
 
+            // Si el contador llega a cierto valor, se busca la clave en la tabla y se muestra el resultado en el JTextArea
             if (cont == 301) {
                 String client = findKey(c.getText());
                 b.setText(client);
             }
-            reader.close();
+
+            reader.close();   // Se cierra el lector
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
+    // Método para obtener la cantidad de elementos almacenados en la tabla
     public int size() {
         int count = 0;
         for (int i = 0; i < capacity; i++) {
