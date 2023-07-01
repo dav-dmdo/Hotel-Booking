6a36654f2bd562b2e6387f9ee76db033eb2120b6
AndresEspositoDe <98119676+AndresEspositoDe@users.noreply.github.com>
2023-07-01 19:02:46 -0400

    ✏️Javadoc

--- HotelBoking2.0/src/DS/BSTree.java
+++ HotelBoking2.0/src/DS/BSTree.java
@@ -8,98 +8,168 @@ import ImportantDataTypes.DataTypeMethods;
 import Nodes.BinaryNode;
 
 /**
- * @see BSTreeMethods.java
+ * La clase BSTree representa un árbol binario de búsqueda genérico. Contiene
+ * métodos para agregar, insertar y buscar elementos en el árbol.
  *
- * @author david
- * 
+ * @param <T> tipo genérico de datos que se utilizará en el árbol.
  */
 public class BSTree<T> {
+
     private BinaryNode<T> root;
     private DataTypeMethods<T> dtm;
 
+    /**
+     * Constructor que recibe un nodo raíz y un objeto DataTypeMethods.
+     *
+     * @param root nodo raíz del árbol.
+     * @param dtm objeto DataTypeMethods utilizado para comparar los datos del
+     * árbol.
+     */
     public BSTree(BinaryNode<T> root, DataTypeMethods<T> dtm) {
         this.root = root;
         this.dtm = dtm;
     }
 
+    /**
+     * Constructor que recibe un objeto DataTypeMethods y crea un árbol vacío
+     * sin nodo raíz.
+     *
+     * @param dtm objeto DataTypeMethods utilizado para comparar los datos del
+     * árbol.
+     */
     public BSTree(DataTypeMethods<T> dtm) {
         this.dtm = dtm;
         this.root = null;
     }
-    
-    public boolean isEmpty(){
+
+    /**
+     * Método que verifica si el árbol está vacío.
+     *
+     * @return true si el árbol está vacío, false en caso contrario.
+     */
+    public boolean isEmpty() {
         return root == null;
     }
-    
-    public void add(T data){
+
+    /**
+     * Método que agrega un nuevo elemento al árbol.
+     *
+     * @param data el elemento que se quiere agregar.
+     */
+    public void add(T data) {
         BinaryNode<T> newNode = new BinaryNode(data);
-        
-        if (isEmpty()){
+
+        if (isEmpty()) {
             root = newNode;
-        }else{
+        } else {
             BinaryNode<T> father = searchParentForInsertion(this.root, data);
-            if (father == null){
-                System.out.println("The element is already parte of the tree.");
-            }else if(dtm.greaterThan(father.data(), data)){
+            if (father == null) {
+                System.out.println("El elemento ya forma parte del árbol.");
+            } else if (dtm.greaterThan(father.data(), data)) {
                 father.leftSon(newNode);
-            }else{
+            } else {
                 father.rightSon(newNode);
             }
-            
+
         }
-                    
+
     }
-    
-    private BinaryNode<T> searchParentForInsertion(BinaryNode<T> root, T data){
+
+    /**
+     * Método privado que busca el padre donde se debe insertar un nuevo
+     * elemento.
+     *
+     * @param root el nodo raíz del subárbol donde se busca el padre.
+     * @param data el elemento que se quiere agregar.
+     * @return el nodo padre donde se debe insertar el nuevo elemento.
+     */
+    private BinaryNode<T> searchParentForInsertion(BinaryNode<T> root, T data) {
         BinaryNode<T> found = null;
-        
-        if (dtm.lessThan(data, root.data())){
-            return processNode(root, root.leftSon(), data);            
-        }else if(dtm.greaterThan(data, root.data())){
+
+        if (dtm.lessThan(data, root.data())) {
+            return processNode(root, root.leftSon(), data);
+        } else if (dtm.greaterThan(data, root.data())) {
             return processNode(root, root.rightSon(), data);
         }
         return found;
     }
-    
-    private BinaryNode<T> processNode(BinaryNode<T> father, BinaryNode<T> son, T data){
-        if (son != null){
+
+    /**
+     * Método privado que procesa cada nodo del subárbol para buscar el padre
+     * donde se debe insertar un nuevo elemento.
+     *
+     * @param father el nodo padre actual.
+     * @param son el nodo hijo actual.
+     * @param data el elemento que se quiere agregar.
+     * @return el nodo padre donde se debe insertar el nuevo elemento.
+     */
+    private BinaryNode<T> processNode(BinaryNode<T> father, BinaryNode<T> son, T data) {
+        if (son != null) {
             return searchParentForInsertion(son, data);
         } else {
             return father;
         }
     }
-    
-    
-    public void insert(T data){
-        root = insert(this.root, data);       
+
+    /**
+     * Método que inserta un nuevo elemento al árbol.
+     *
+     * @param data el elemento que se quiere agregar.
+     */
+    public void insert(T data) {
+        root = insert(this.root, data);
     }
-    
-    private BinaryNode<T> insert(BinaryNode<T> root, T data){
-        if(root == null){
-            root = new BinaryNode<> (data);
-            return root;            
+
+    /**
+     * Método privado que inserta un nuevo elemento al subárbol.
+     *
+     * @param root el nodo raíz del subárbol.
+     * @param data el elemento que se quiere agregar.
+     * @return el nodo raíz del subárbol actualizado.
+     */
+    private BinaryNode<T> insert(BinaryNode<T> root, T data) {
+        if (root == null) {
+            root = new BinaryNode<>(data);
+            return root;
         }
-        if(dtm.lessThan(data, root.data())){
+        if (dtm.lessThan(data, root.data())) {
             root.leftSon(insert(root.leftSon(), data));
-        }else{
+        } else {
             root.rightSon(insert(root.rightSon(), data));
         }
         return root;
     }
-    
-    public BinaryNode<T> search(T data){
+
+    /**
+     * Método que busca un elemento en el árbol.
+     *
+     * @param data el elemento que se quiere buscar.
+     * @return el nodo que contiene el elemento buscado, null si no se
+     * encuentra.
+     */
+    public BinaryNode<T> search(T data) {
         return search(this.root, data);
     }
-    
-    private BinaryNode<T> search(BinaryNode<T> root, T data){
-        if ((root == null) || (dtm.isEqual(root.data(), data)))
-            return root;        
-        if(dtm.lessThan(data, root.data()))
+
+    /**
+     * Método privado que busca unelemento en el subárbol.
+     *
+     * @param root el nodo raíz del subárbol.
+     * @param data el elemento que se quiere buscar.
+     * @return el nodo que contiene el elemento buscado, null si no se
+     * encuentra.
+     */
+    private BinaryNode<T> search(BinaryNode<T> root, T data) {
+        if ((root == null) || (dtm.isEqual(root.data(), data))) {
+            return root;
+        }
+        if (dtm.lessThan(data, root.data())) {
             return search(root.leftSon(), data);
-        else
+        } else {
             return search(root.rightSon(), data);
-    }    
-    
+        }
+    }
+
     /**
      * @return the root
      */
@@ -120,9 +190,5 @@ public class BSTree<T> {
     public DataTypeMethods<T> dtm() {
         return dtm;
     }
-    
-    
-    
-    
-    
+
 }
--- HotelBoking2.0/src/DS/HashTable.java
+++ HotelBoking2.0/src/DS/HashTable.java
@@ -9,7 +9,13 @@ import Nodes.DoubleNode;
 import static ControllerAndRelated.DoubleLinkedListMethods.*;
 
 /**
+ * Clase que implementa una Hash Table, una tabla Hash, que utiliza una
+ * estructura de datos de lista doblemente enlazada para manejar los elementos
+ * de la tabla. La tabla tiene un tamaño fijo determinado por la capacidad. La
+ * tabla Hash utiliza una función hash para calcular la posición de los
+ * elementos en la tabla.
  *
+ * @param <T> tipo de dato que se almacena en la tabla
  * @author david
  * @param <T>
  */
@@ -21,6 +27,15 @@ public class HashTable<T> {
     private final DataTypeMethods<T> dtm;
     private final boolean keyType; //True means key type is String
 
+    /**
+     *
+     * Constructor de la clase HashTable
+     *
+     * @param capacity tamaño de la tabla Hash
+     * @param dtm objeto DataTypeMethods que contiene los métodos para obtener
+     * las claves numéricas o de texto
+     * @param keyType indica el tipo de clave (true si es String)
+     */
     public HashTable(Integer capacity, DataTypeMethods<T> dtm, boolean keyType) {
         this.size = 0;
         this.capacity = capacity;
@@ -33,10 +48,11 @@ public class HashTable<T> {
     }
 
     /**
+     * Inserta un nuevo elemento en la tabla Hash, calculando su posición
+     * mediante una función hash.
      *
      * @param data
      */
-
     public void insert(T data) {
         Integer key = getKey(data);
         DoubleLinkedList<T> list = table[key];
@@ -45,51 +61,58 @@ public class HashTable<T> {
     }
 
     /**
-     *
-     * @param key
-     * @return 
-     */
-
-    public DoubleNode<T> delete(String key) {
-        DoubleNode<T> aux= null;
-        Integer hash = hashFunction(key);
-        DoubleLinkedList<T> list = table[hash];
-        if(!list.isEmpty()){
-            System.out.println(list.dtm().toString(list.head().data()));
-            aux = searchStringKey(list, key);
-            
-        }
-        if (aux != null){
-            deleteByStringKey(list, key);
-            size--;
-        }
-        
-        
-        return aux;
-    }
-
-    /**
+     * Busca y elimina un elemento en la tabla Hash utilizando su clave (key).
      *
      * @param key
      * @return
      */
+    public DoubleNode<T> delete(String key) {
+        DoubleNode<T> aux = null;
+        Integer hash = hashFunction(key);
+        DoubleLinkedList<T> list = table[hash];
+        if (!list.isEmpty()) {
+            System.out.println(list.dtm().toString(list.head().data()));
+            aux = searchStringKey(list, key);
 
+        }
+        if (aux != null) {
+            deleteByStringKey(list, key);
+            size--;
+        }
+
+        return aux;
+    }
+
+    /**
+     * Busca un elemento en la tabla Hash utilizando su clave (key)
+     *
+     * @param key clave del elemento a buscar
+     * @return DoubleNode que contiene el elemento buscado, o null si no se
+     * encuentra el elemento.
+     */
 //    public DoubleLinkedList<T> searchByKey(Object key) {
 //        Integer hash = (keyType) ? hashFunction((String) key) : hashFunction((Integer) key);
 //        DoubleLinkedList<T> list = table[hash];
 //        return list;
 //    }
-
-    public DoubleNode<T> search(String key){
+    public DoubleNode<T> search(String key) {
         DoubleNode<T> aux = null;
         Integer hash = this.hashFunction(key);
         DoubleLinkedList<T> list = table[hash];
-        if (!list.isEmpty()){
+        if (!list.isEmpty()) {
             aux = searchStringKey(list, key);
         }
         return aux;
     }
-    
+
+    /**
+     *
+     * Calcula la clave del elemento (data) que se va a insertar en la tabla
+     * Hash.
+     *
+     * @param data el elemento a insertar
+     * @return la clave numérica o de texto correspondiente al elemento (data)
+     */
     private Integer getKey(T data) {
         if (keyType) {
             String key = getStringKey(data);
@@ -99,32 +122,78 @@ public class HashTable<T> {
         return hashFunction(key);
     }
 
+    /**
+     *
+     * Obtiene la clave numérica correspondiente al elemento (data).
+     *
+     * @param data el elemento del cual se va a obtener la clave numérica
+     * @return la clave numérica correspondiente al elemento (data)
+     */
     private Integer getNumericalKey(T data) {
         return dtm().getNumericalKey(data);
     }
 
+    /**
+     *
+     * Obtiene la clave de texto correspondiente al elemento (data).
+     *
+     * @param data el elemento del cual se va a obtener la clave de texto
+     * @return la clave de texto correspondiente al elemento (data)
+     */
     private String getStringKey(T data) {
         return dtm().getStringKey(data);
     }
 
+    /**
+     *
+     * Calcula un valor disperso utilizando la clave de texto.
+     *
+     * @param key clave de texto del elemento que se va a insertar en la tabla
+     * Hash
+     * @return un valor disperso calculado a partir de la clave de texto
+     */
     private Integer disperse(String key) {
         Integer sum = 0;
         char[] chars = key.toCharArray();
-        for (int i = chars.length-1; i >= 0; i--) {
-            sum += chars[i] * (1+i);
+        for (int i = chars.length - 1; i >= 0; i--) {
+            sum += chars[i] * (1 + i);
         }
         return sum;
     }
 
+    /**
+     *
+     * Aplica la función hash a la clave de texto del elemento a insertar.
+     *
+     * @param key clave de texto del elemento a insertar
+     * @return la posición en la tabla Hash correspondiente al elemento a
+     * insertar
+     */
     public Integer hashFunction(String key) {
         return (disperse(key) % capacity);
     }
 
+    /**
+     *
+     * Calcula un valor disperso utilizando la clave numérica.
+     *
+     * @param key clave numérica del elemento que se va a insertar en la tabla
+     * Hash
+     * @return un valor disperso calculado a partir de la clave numérica
+     */
     private Integer disperse(Integer key) {
         Integer sum = key;
         return sum;
     }
 
+    /**
+     *
+     * Aplica la función hash a la clave numérica del elemento a insertar.
+     *
+     * @param key clave numérica del elemento a insertar
+     * @return la posición en la tabla Hash correspondiente al elemento a
+     * insertar
+     */
     public Integer hashFunction(Integer key) {
         return (disperse(key) % capacity);
     }
@@ -143,6 +212,4 @@ public class HashTable<T> {
         return table;
     }
 
-    
-    
 }
